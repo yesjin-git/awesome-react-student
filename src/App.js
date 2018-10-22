86f3fa11923d51fc331a624f60abcba9dd58cee8
Yesjin <yesjin_nav@naver.com>
2018-10-22 16:58:42 +0900

    Week1 Homework 1.2 Import Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
