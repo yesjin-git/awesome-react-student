845f5cb910910fd8e4bb2217bbc17a2424b3b779
Yesjin <yesjin_nav@naver.com>
2018-10-22 17:00:56 +0900

    Week2 Homework 2.1 Make Navbar

--- src/App.js
+++ src/App.js
@@ -8,9 +8,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">ITube</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
