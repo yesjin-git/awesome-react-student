784dc4f47dcaf4d0408bc3c5e93e7b43d57f2b34
Eungjin <tiuln2010@gmail.com>
2018-12-28 15:56:40 +0900

    week3 material 3. proptypes example in Note

--- 3week/keep2/src/Note.js
+++ 3week/keep2/src/Note.js
@@ -1,17 +1,22 @@
 import React,{Component} from "react"
+import PropTypes from 'prop-types'
 
 class Note extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }
   render() {
-		const title = this.props.title
+    const title = this.props.title
     const content = this.props.content
     return (
       //아래 내용들은 materialize에 있는 라이브러리와 클래스를 활용한 것 입니다.
       //materialize 의 grid부분을 참고해 주세요.
       <div className='col s12 m6 l3'>
         <div className='card yellow lighten-4'>
-					<span className='card-title'>
-						{title}
-					</span>
+          <span className='card-title'>
+            {title}
+          </span>
           <div className='card-content black-text'>
             {content}
           </div>
@@ -20,5 +25,6 @@ class Note extends Component {
     )
   }
 }
+
 
 export default Note
