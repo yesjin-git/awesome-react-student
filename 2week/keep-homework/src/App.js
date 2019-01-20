c01d1c14da18a7860f542226c668efe678c5340a
meerkat9 <tmdghk05@gmail.com>
2019-01-20 15:20:46 +0900

    과제 2 keep 구현

--- 2week/keep-homework/src/App.js
+++ 2week/keep-homework/src/App.js
@@ -7,19 +7,32 @@ class App extends Component {
     super(props)
     this.state = {
       savedNotes: [
-        {content: "default1"},
-        {content: "default2"},
-        {content: "default3"}
+        {
+          id: 1,
+          title: 'title1',
+          content: "default1"
+        },
+        {
+          id: 2,
+          title: 'title2',
+          content: "default2"
+        },
+        {
+          id:3,
+          title: 'title3',
+          content: "default3"
+        },
       ]
     }
   }
 
   save = (userInput) => {
-    const savedNotes = this.state.savedNotes
+    const savedNotes = this.state.savedNotes;
+    const id = savedNotes.length + 1;
     this.setState({
       savedNotes: [
         ...savedNotes, 
-        {content: userInput}
+        {id, ...userInput}
       ]
     })
   }
@@ -30,7 +43,7 @@ class App extends Component {
         <Writing save={this.save} />
         <div className='row'>
           {this.state.savedNotes.map((note) => (
-            <Note content={note.content} />
+            <Note title={note.title} content={note.content} key={note.id} />
           ))}
         </div>
       </div>
@@ -41,33 +54,48 @@ class App extends Component {
 class Writing extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      userInput: "test"
+    this.state = {     
+      title: 'title',
+      content: 'test'     
     }
   }
 
   handleSubmit = (e) => {
-    console.log('submitted')
-    this.props.save(this.state.userInput)
+    console.log('submitted');
+    this.props.save(this.state);
+    // 입력값 초기화
+    this.setState({     
+      title: '',
+      content: ''     
+    });
     e.preventDefault();
   }
 
-  handleChange = (event) => {
-    console.log('userInput is ' + this.state.userInput)
-    this.setState({
-      userInput: event.target.value
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({      
+      [name]: value
     })
   }
 
   render() {
+    const { handleSubmit, handleChange } = this;
+    const { title, content } = this.state;
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="input-field">
             <input
               type='text'
-              value={this.state.userInput}
-              onChange={this.handleChange}
+              name='title'
+              value={title}
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              name='content'
+              value={content}
+              onChange={handleChange}
             />
           </div>
           <input
@@ -82,12 +110,15 @@ class Writing extends Component {
 
 class Note extends Component {
   render() {
-    const content = this.props.content
+    const {title, content} = this.props;
     return (
       //아래 내용들은 materialize에 있는 라이브러리와 클래스를 활용한 것 입니다.
       //materialize 의 grid부분을 참고해 주세요.
       <div className='col s12 m6 l3'>
         <div className='card yellow lighten-4'>
+          <div className='card-title black-text'>
+            {title}
+          </div>
           <div className='card-content black-text'>
             {content}
           </div>
