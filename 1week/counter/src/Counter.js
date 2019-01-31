4664d12a43cb3fc781cf839fe419063b6f828490
colorbrick <colorbrick@naver.com>
2019-01-31 13:15:47 +0900

    과제 1주차 구현

--- 1week/counter/src/Counter.js
+++ 1week/counter/src/Counter.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-//react를 사용하기 위해서 react 라이브러리에서 React와 Component를 호출함
+import React, { Component } from "react";
 
 //Counter 컴포넌트를 생성 하고 Component를 상속
 class Counter extends Component {
@@ -9,11 +8,10 @@ class Counter extends Component {
     return (
       //두개 이상의 엘리먼트를 return 할때는 반드시 div로 감싸야 한다.
       <div>
-        <h1>Counter</h1>
-        <div>값: {this.props.number}</div>
-        <button onClick={this.props.handleIncrease}>+</button>
-        <button onClick={this.props.handleDecrease}>-</button>
-        <button onClick={this.props.handleReset}>reset</button>
+        <p>{this.props.num}</p>
+        <button onClick={this.props.add}>+</button>
+        <button onClick={this.props.minus}>-</button>
+        <button onClick={this.props.reset}>reset</button>
       </div>
     );
   }
