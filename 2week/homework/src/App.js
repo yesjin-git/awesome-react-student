4168f4379f199e4eb4dd0ef197a34035e0f8609f
tiuln2010 <destiy@naver.com>
2019-03-14 14:30:03 +0900

    Replace homework folder from week3

---
+++ 2week/homework/src/App.js
@@ -0,0 +1,63 @@
+import React, {Component} from "react"
+
+import Writing from "./Writing"
+import Note from "./Note"
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      savedNotes: [
+        {id: 0, title: "title1", content: "default1"},
+        {id: 1, title: "title2", content: "default2"},
+        {id: 2, title: "title3", content: "default3"}
+      ]
+    }
+  }
+
+  save = (writingState) => {
+    const {savedNotes} = this.state
+    const lastNoteId = savedNotes[savedNotes.length - 1].id
+
+    this.setState({
+      savedNotes: [
+        ...savedNotes,
+        {
+          id: lastNoteId + 1,
+          title: writingState.title,
+          content: writingState.content
+        }
+      ]
+    })
+  }
+
+  delete = (index) => {
+    console.log(`${index} will be deleted`)
+    const {savedNotes} = this.state
+    savedNotes.splice(index, 1)
+    this.setState({
+      savedNotes: savedNotes
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Writing save={this.save} />
+        <div className='row'>
+          {this.state.savedNotes.map((note, index) => (
+            <Note
+              delete={this.delete}
+              title={note.title}
+              content={note.content}
+              index={index}
+              key={note.id}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ 2week/homework/src/Note.js
@@ -0,0 +1,46 @@
+import React, {Component} from "react"
+import "./note.css"
+// import PropTypes from "prop-types"
+
+class Note extends Component {
+  handleClickDelete = () => {
+    this.props.delete(this.props.index)
+  }
+
+  render() {
+    const {title, content} = this.props
+    return (
+      //아래 내용들은 materialize에 있는 라이브러리와 클래스를 활용한 것 입니다.
+      //materialize 의 grid부분을 참고해 주세요.
+      <div className='Note col s12 m4 l3'>
+        <div className='DeleteBtn'>
+          <div className='DeleteBtn btn-floating btn-large'>
+            <i onClick={this.handleClickDelete} id='Icon' className='material-icons'>
+              delete
+            </i>
+          </div>
+        </div>
+        <div className='card yellow lighten-4'>
+          <div className='card-content black-text'>
+            <span className='card-title'>{title}</span>
+            <p>{content}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function inputIsRequired(props, propName, componentName) {
+  if (!props[propName]) {
+    return new Error(`${propName} is required`)
+  }
+  return null
+}
+
+Note.propTypes = {
+  title: inputIsRequired,
+  content: inputIsRequired
+}
+
+export default Note
