b0ba6fc1467dfcdd9db0dd8fa6140da2d5e668c3
claire0130 <claire10040130@gmail.com>
2019-07-19 23:48:02 +0900

    Modify. data값을 자식 Counter.js로이동

--- 1week/class/src/App.js
+++ 1week/class/src/App.js
@@ -1,76 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Counter from './Counter';
-import Clock from './Clock';
-import './App.css';
 
-class App extends Component {
-  state = {
-    initNum : 0,
-    countNum : 1,
-    countNum2 : 2,
-    date: new Date().toLocaleTimeString()
-  }
-  
-  componentDidMount() {
-    this.intervalID = setInterval(() => this.time(), 1000);
-  }
-
-  handleInc = (countNum) => {
-    const { initNum } = this.state;
-
-    this.setState({
-      initNum : initNum + countNum
-    });
-  }
-
-  handleDec = (countNum) => {
-    const { initNum } = this.state;
-
-    this.setState({
-      initNum : initNum - countNum
-    });
-  }
-
-  handleReset = () => {
-    this.setState({
-      initNum : 0
-    });
-  }
-  
-  time = () => {
-    this.setState({
-      date: new Date().toLocaleTimeString()
-    });
-  }
-
-  startClock = () => {
-    this.setState({
-      date : setInterval(() => this.time(), 1000)
-    });
-  }
-
-  stopClock = () => {
-    clearInterval(this.intervalID);
-  }
-  
-  render() {
-    const { handleInc , handleDec, handleReset, startClock, stopClock } = this;
-    const { initNum, date, countNum, countNum2 } = this.state;
-    
-    return (
-      <div className="section_main">
-        <h2>Counter 1씩 증감</h2>
-        <Counter initNum={initNum} handleInc={handleInc} handleDec={handleDec} handleReset={handleReset} countNum={countNum} />
-        
-        <hr />
-        <h2>Counter 2씩 증감</h2>
-        <Counter initNum={initNum} handleInc={handleInc} handleDec={handleDec} handleReset={handleReset} countNum={countNum2} />
-        
-        <hr />
-        <Clock date={date} startClock={startClock} stopClock={stopClock} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="section_main">
+      <h2>Counter 1씩 증감</h2>
+      <Counter interval={1} />
+      
+      <hr />
+      <h2>Counter 2씩 증감</h2>
+      <Counter interval={2} />
+    </div>
+  );
+};
 
 export default App;
--- 1week/class/src/Counter.js
+++ 1week/class/src/Counter.js
@@ -1,20 +1,28 @@
 import React, { Component, Fragment } from 'react';
+import './Counter.css';
 
 class Counter extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    if(this.props.initNum === nextProps.initNum) return false;
-    return true;
+  state = {
+    initNum : 0
+  }
+
+  setNumber = (num)=>{
+    this.setState({
+      initNum : num
+    });
   }
   
   render() {
-    const { initNum, countNum, handleInc , handleDec, handleReset } = this.props;
+    const { setNumber } = this;
+    const { initNum } = this.state;
+    const { interval } = this.props;
     
     return (
       <Fragment>
         <p className="txt_result">값: {initNum}</p>
-        <button className="btn_comm" onClick={() => handleInc(countNum)}>증감 +</button>
-        <button className="btn_comm" onClick={() => handleDec(countNum)}>감소 -</button>
-        <button className="btn_comm" onClick={handleReset}>초기화</button>
+        <button className="btn_comm" onClick={() => setNumber(interval + initNum)}>증감 +</button>
+        <button className="btn_comm" onClick={() => setNumber(initNum - interval)}>감소 -</button>
+        <button className="btn_comm" onClick={() => setNumber(0)}>초기화</button>
       </Fragment>
     );
   }
