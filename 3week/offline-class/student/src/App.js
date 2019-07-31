42068bdb50658b81326ed46333886e2849349394
dwnusa <dwnusa@gmail.com>
2019-07-31 17:10:18 +0900

    update all with offline-class folder

---
+++ 3week/offline-class/student/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Modal from "./Modal";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Modal>
+          alksjdfl;kajsdlkjaslkjl;k
+        </Modal>
+      </header>
+    </div>
+  );
+}
+
+export default App;
