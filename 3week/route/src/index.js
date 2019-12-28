803b706f29fe482f6c234ea015e2a9e6a11bfc50
evans <joshua@coupang.com>
2019-12-28 18:55:38 +0900

    [과제 3] 수업내용 업데이트

---
+++ 3week/route/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom"
+
+ReactDOM.render(
+    //App 컴포넌트 아래에서 모두 Router를 사용할 수 있도록 한다.
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
